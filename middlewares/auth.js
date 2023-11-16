5caaf884845d644acd4417ba9b7fab8b79c5ec09
Mahdi <mahdizareiofficial@gmail.com>
2023-11-16 16:15:50 +0330

    Dev auth middleware

---
+++ middlewares/auth.js
@@ -0,0 +1,28 @@
+const usersModel = require("../models/user");
+const jwt = require("jsonwebtoken");
+
+module.exports = async (req, res, next) => {
+  const authHeader = req.header("Authorization").split(" ");
+
+  if (authHeader.length !== 2) {
+    return res
+      .status(403)
+      .json({ message: "You don`t have access to th route !!" });
+  }
+
+  const token = authHeader[1];
+
+  try {
+    const jwtPayload = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await usersModel.findById(jwtPayload.id).lean();
+
+    Reflect.deleteProperty(user, "password");
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    return res.json(error);
+  }
+};
