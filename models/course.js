8fd55e9c9bf0675e41fd80217f965ba5cf3ddaaa
Mahdi <mahdizareiofficial@gmail.com>
2023-11-25 20:12:50 +0330

    feat:Dev course model

---
+++ models/course.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    creator: {
+      type: mongoose.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    categoryID: {
+      type: mongoose.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    support: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+schema.virtual("comments", {
+  ref: "Comment",
+  localField: "_id",
+  foreignField: "course",
+});
+
+schema.virtual("sessions", {
+  ref: "Sessions",
+  localField: "_id",
+  foreignField: "course",
+});
+
+const coursesModel = mongoose.model("Course", schema);
+
+module.exports = coursesModel;
