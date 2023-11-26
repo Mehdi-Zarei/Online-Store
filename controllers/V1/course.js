1be60008e9f46f7fe23e7112943885ea0f09bc86
Mahdi <mahdizareiofficial@gmail.com>
2023-11-26 20:32:24 +0330

    feat:coding create course

---
+++ controllers/V1/course.js
@@ -0,0 +1,41 @@
+const courseModel = require("../../models/course");
+
+exports.create = async (req, res) => {
+  const {
+    name,
+    status,
+    descriptions,
+    categoryID,
+    href,
+    support,
+    price,
+    discount,
+  } = req.body;
+
+  const courseExist = await courseModel.findOne({
+    $or: [{ name: req.body.name }, { href: req.body.href }],
+  });
+
+  if (courseExist) {
+    return res.status(409).json({ message: "This course already exists!!" });
+  }
+
+  const course = await courseModel.create({
+    name,
+    status,
+    descriptions,
+    cover: req.file.filename,
+    creator: req.user._id,
+    categoryID,
+    href,
+    support,
+    price,
+    discount,
+  });
+
+  const mainCourse = await courseModel
+    .findById(course._id)
+    .populate("creator", "-password -__v");
+
+  return res.status(201).json(mainCourse);
+};
