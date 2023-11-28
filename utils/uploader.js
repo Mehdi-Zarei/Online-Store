55fe51592cced515939a65e8d34f2abc84f35370
Mahdi <mahdizareiofficial@gmail.com>
2023-11-28 21:24:56 +0330

    feat:Dev destinationFolder code

--- utils/uploader.js
+++ utils/uploader.js
@@ -1,11 +1,28 @@
 const multer = require("multer");
-const crypto = require("crypto");
 const path = require("path");
+// const crypto = require("crypto")
 
 module.exports = multer.diskStorage({
-  destination: (req, res, cb) => {
-    cb(null, path.join(__dirname, "..", "public", "courses", "covers"));
+  destination: (req, file, cb) => {
+    const ext = path.extname(file.originalname);
+    let destinationFolder;
+
+    if (ext === ".jpg" || ext === ".jpeg" || ext === ".png") {
+      destinationFolder = "covers";
+    } else if (ext === ".mp4" || ext === ".avi" || ext === ".mkv") {
+      destinationFolder = "videos";
+    } else {
+      return cb(
+        "Unsupported file format. Only 'mp4', 'avi', and 'mkv' formats are supported for videos, and 'jpg', 'jpeg', and 'png' formats for images."
+      );
+    }
+
+    cb(
+      null,
+      path.join(__dirname, "..", "public", "courses", destinationFolder)
+    );
   },
+
   filename: (req, file, cb) => {
     const fileName = Date.now() + String(Math.random(Math.random()));
 
