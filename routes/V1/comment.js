45cc80f6ae2e88b44cfb970d780dd37c728817e4
Mahdi <mahdizareiofficial@gmail.com>
2023-12-03 14:13:05 +0330

    feat:coding remove comment route

--- routes/V1/comment.js
+++ routes/V1/comment.js
@@ -2,7 +2,12 @@ const express = require("express");
 const commentsRouter = express.Router();
 const authMiddleware = require("../../middlewares/auth");
 const commentsController = require("../../controllers/V1/comment");
+const isAdminMiddleware = require("../../middlewares/isAdmin");
 
 commentsRouter.route("/").post(authMiddleware, commentsController.create);
+
+commentsRouter
+  .route("/:id")
+  .delete(authMiddleware, isAdminMiddleware, commentsController.remove);
 
 module.exports = commentsRouter;
