13205ba92e48345dab257b43cbe8a96a37a0ad04
Mahdi <mahdizareiofficial@gmail.com>
2023-12-05 21:17:40 +0330

    feat:set timeStamps

--- models/contact.js
+++ models/contact.js
@@ -1,27 +1,30 @@
 const mongoose = require("mongoose");
 
-const schema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    body: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    isAnswer: {
+      type: Number, // 0 - 1
+      required: true,
+    },
   },
-  body: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-  },
-  phone: {
-    type: String,
-    required: true,
-  },
-  isAnswer: {
-    type: Number, // 0 - 1
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
 const model = mongoose.model("Contact", schema);
 
