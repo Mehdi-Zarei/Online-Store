7015aebaa5d6f29bfd2f953184baf235a3962628
Mahdi <mahdizareiofficial@gmail.com>
2023-12-08 01:44:23 +0330

    feat:coding answer to contact msg func

--- controllers/V1/contact.js
+++ controllers/V1/contact.js
@@ -1,5 +1,6 @@
 const { isValidObjectId, default: mongoose } = require("mongoose");
 const contactModel = require("../../models/contact");
+const nodemailer = require("nodemailer");
 
 exports.getAll = async (req, res) => {
   const contacts = await contactModel.find({}).lean();
@@ -9,12 +10,12 @@ exports.getAll = async (req, res) => {
 exports.create = async (req, res) => {
   const { name, body, email, phone } = req.body;
 
-  await contactModel.create({
+  const newContact = await contactModel.create({
     name,
     email,
     phone,
     body,
-    isAnswer: 0,
+    answer: 0,
   });
   return res.status(201).json({ message: "Your message sent successfully." });
 };
@@ -38,4 +39,33 @@ exports.remove = async (req, res) => {
     .json({ message: "Contact message removed successfully." });
 };
 
-exports.answer = async (req, res) => {};
+exports.answer = async (req, res) => {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "test@gmail.com", // This entry must be changed to send an email.
+      pass: "testPassword", // This entry must be changed to send an email.
+    },
+  });
+
+  const mailOptions = {
+    from: "test@gmail.com", // This entry must be changed to send an email.
+    to: req.body.email,
+    subject: "پاسخ پیام شما",
+    text: req.body.answer,
+  };
+
+  transporter.sendMail(mailOptions, async (err, info) => {
+    if (err) {
+      return res.status(502).json({ message: err });
+    } else {
+      const updateContactInfo = await contactModel.findOneAndUpdate(
+        { email: req.body.email },
+        {
+          answer: 1,
+        }
+      );
+      return res.json({ message: "Email send successfully." });
+    }
+  });
+};
