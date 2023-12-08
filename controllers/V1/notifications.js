a810017d70039c025685b51c10953757bb2b4b81
Mahdi <mahdizareiofficial@gmail.com>
2023-12-08 21:49:22 +0330

    feat:create notifications funcs

---
+++ controllers/V1/notifications.js
@@ -0,0 +1,13 @@
+const notificationsModel = require("../../models/notifications");
+
+exports.create = async (req, res) => {
+  const { admin, message } = req.body;
+};
+
+exports.get = async (req, res) => {};
+
+exports.seen = async (req, res) => {};
+
+exports.remove = async (req, res) => {};
+
+exports.getAll = async (req, res) => {};
