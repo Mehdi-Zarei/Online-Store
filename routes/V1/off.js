b91c97be5bee80d77e9548ef39b80a8a3920414d
Mahdi <mahdizareiofficial@gmail.com>
2023-12-09 20:57:41 +0330

    feat:coding off routes

---
+++ routes/V1/off.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const offRouter = express.Router();
+const offController = require("../../controllers/V1/off");
+const authMiddleware = require("../../middlewares/auth");
+const isAdminMiddleware = require("../../middlewares/isAdmin");
+
+offRouter
+  .route("/")
+  .get(authMiddleware, isAdminMiddleware, offController.getAll)
+  .post(authMiddleware, isAdminMiddleware, offController.create);
+
+offRouter
+  .route("/all")
+  .post(authMiddleware, isAdminMiddleware, offController.setOnAll);
+
+offRouter
+  .route("/:code")
+  .post(authMiddleware, isAdminMiddleware, offController.getOn)
+  .delete(authMiddleware, isAdminMiddleware, offController.remove);
+
+module.exports = offRouter;
