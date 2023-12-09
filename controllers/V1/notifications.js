6c23fda26a74d9326d8acaff1abde4ad25d4dbd3
Mahdi <mahdizareiofficial@gmail.com>
2023-12-09 19:52:40 +0330

    feat:coding notifications funcs(create,getAll,remove)

--- controllers/V1/notifications.js
+++ controllers/V1/notifications.js
@@ -1,13 +1,49 @@
+const { default: mongoose } = require("mongoose");
 const notificationsModel = require("../../models/notifications");
+const notificationsValidator = require("../../validators/notifications");
 
 exports.create = async (req, res) => {
   const { admin, message } = req.body;
+
+  const validatorResult = notificationsValidator(req.body);
+
+  if (validatorResult != true) {
+    return res.status(422).json(validatorResult);
+  }
+
+  const newNotification = await notificationsModel.create({ message, admin });
+
+  return res.status(201).json({ message: "Notification send successfully." });
 };
 
 exports.get = async (req, res) => {};
 
+exports.getAll = async (req, res) => {
+  const allNotifications = await notificationsModel.find({}).lean();
+  return res.json(allNotifications);
+};
+
 exports.seen = async (req, res) => {};
 
-exports.remove = async (req, res) => {};
+exports.remove = async (req, res) => {
+  const checkObjectIdResult = mongoose.Types.ObjectId.isValid(req.params);
 
-exports.getAll = async (req, res) => {};
+  if (checkObjectIdResult != true) {
+    return res.status(409).json({ message: "ObjectId not valid !" });
+  }
+
+  const remove = await notificationsModel.findOneAndDelete({
+    _id: req.params.id,
+  });
+
+  if (!remove) {
+    return res
+      .status(404)
+      .json({ message: "Notification message not found !" });
+  }
+
+  return res.json({
+    message: "Notification message removed successfully.",
+    remove,
+  });
+};
