4b0a120f2f2535a88aaa85ca7c484c30736258fd
Mahdi <mahdizareiofficial@gmail.com>
2023-12-10 22:07:21 +0330

    feat:coding articles router

---
+++ routes/V1/article.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const articlesRouter = express.Router();
+const articlesController = require("../../controllers/V1/article");
+const authMiddleware = require("../../middlewares/auth");
+const isAdminMiddleware = require("../../middlewares/isAdmin");
+const multer = require("multer");
+const multerStorage = require("../../utils/uploader");
+
+articlesRouter
+  .route("/")
+  .get(articlesController.getAll)
+  .post(
+    authMiddleware,
+    isAdminMiddleware,
+    multer({ storage: multerStorage }),
+    articlesController.create
+  );
+
+articlesRouter.route("/:href").get(articlesController.getOne);
+
+articlesRouter
+  .route("/:id")
+  .delete(authMiddleware, isAdminMiddleware, articlesController.remove);
+
+articlesRouter
+  .route("/draft")
+  .post(
+    authMiddleware,
+    isAdminMiddleware,
+    multer({ storage: multerStorage }),
+    articlesController.saveDraft
+  );
+
+module.exports = articlesRouter;
