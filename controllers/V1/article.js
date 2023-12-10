e57c2e5abafdf2768ee213594731c1ad3b66926b
Mahdi <mahdizareiofficial@gmail.com>
2023-12-10 22:06:18 +0330

    feat:coding articles funcs

---
+++ controllers/V1/article.js
@@ -0,0 +1,11 @@
+const articlesModel = require("../../models/article");
+
+exports.getAll = async (req, res) => {};
+
+exports.create = async (req, res) => {};
+
+exports.getOne = async (req, res) => {};
+
+exports.remove = async (req, res) => {};
+
+exports.saveDraft = async (req, res) => {};
