2d4903fcf07a5aca76e3aefc8d2f6cc86417eec0
Mahdi <mahdizareiofficial@gmail.com>
2023-12-10 18:39:22 +0330

    feat:coding create and remove func

---
+++ controllers/V1/off.js
@@ -0,0 +1,39 @@
+const { json } = require("express");
+const courseModel = require("../../models/course");
+const offModel = require("../../models/off");
+
+exports.setOnAll = async (req, res) => {
+  const { discount } = req.body;
+
+  const setDiscount = await courseModel.updateMany({ discount });
+
+  return res.json({ message: "Discount set successfully on all courses." });
+};
+
+exports.getAll = async (req, res) => {};
+
+exports.create = async (req, res) => {
+  const { code, max, course, percent } = req.body;
+  const newOff = await offModel.create({
+    code,
+    course,
+    max,
+    percent,
+    uses: 0,
+    creator: req.user._id,
+  });
+  return res
+    .status(201)
+    .json({ message: "New discount code created successfully." });
+};
+
+exports.getOn = async (req, res) => {};
+
+exports.remove = async (req, res) => {
+  const { code } = req.params;
+  const remove = await offModel.findOneAndDelete({ code });
+  if (!remove) {
+    return res.status(404).json({ message: "Discount code not found !" });
+  }
+  return res.json(remove);
+};
