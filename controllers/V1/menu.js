5935c1d91ffe6ecacdd0819274a48c7801c6b20d
Mahdi <mahdizareiofficial@gmail.com>
2023-12-15 20:15:30 +0330

    feat:create menu controller file with funcs

---
+++ controllers/V1/menu.js
@@ -0,0 +1,11 @@
+const menuModel = require("../../models/menu");
+
+exports.getAll = async (req, res) => {};
+
+exports.create = async (req, res) => {};
+
+exports.adminGetAll = async (req, res) => {};
+
+exports.remove = async (req, res) => {};
+
+exports.updateMenuName = async (req, res) => {};
