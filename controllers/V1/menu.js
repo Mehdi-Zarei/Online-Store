e7d910664ffc01185f2c806e069f2ab0dc62ab10
Mahdi <mahdizareiofficial@gmail.com>
2023-12-16 16:04:36 +0330

    feat:coding getAll function with two different methods

--- controllers/V1/menu.js
+++ controllers/V1/menu.js
@@ -1,6 +1,42 @@
 const menuModel = require("../../models/menu");
 
-exports.getAll = async (req, res) => {};
+// exports.getAll = async (req, res) => {
+//   //! This func use for and forEach loop
+//   const menus = await menuModel.find({}).lean();
+
+//   menus.forEach((menu) => {
+//     const submenus = [];
+//     for (let i = 0; i < menus.length; i++) {
+//       const mainMenu = menus[i];
+//       if (mainMenu.parent?.equals(menu._id)) {
+//         submenus.push(menus.splice(i, 1)[0]); // trace => support
+//         i = i - 1;
+//       }
+//     }
+//     menu.submenus = submenus;
+//   });
+
+//   return res.json(menus);
+// };
+
+exports.getAll = async (req, res) => {
+  //! This func use forEach loop and filter method
+
+  const menus = await menuModel.find({}).lean();
+
+  const result = [];
+
+  menus.forEach((menu) => {
+    if (!menu.parent) {
+      const submenus = menus.filter(
+        (mainMenu) => String(mainMenu.parent) === String(menu._id)
+      );
+      result.push({ ...menu, submenus });
+    }
+  });
+
+  return res.json(result);
+};
 
 exports.create = async (req, res) => {
   const { title, href, parent } = req.body;
