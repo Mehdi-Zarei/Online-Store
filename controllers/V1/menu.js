fa8dc8955be2cf1102b7f4e7474253b51a824024
Mahdi <mahdizareiofficial@gmail.com>
2023-12-16 13:34:23 +0330

    feat:coding create menu func

--- controllers/V1/menu.js
+++ controllers/V1/menu.js
@@ -2,7 +2,17 @@ const menuModel = require("../../models/menu");
 
 exports.getAll = async (req, res) => {};
 
-exports.create = async (req, res) => {};
+exports.create = async (req, res) => {
+  const { title, href, parent } = req.body;
+
+  const menu = await menuModel.create({
+    title,
+    href,
+    parent,
+  });
+
+  return res.status(201).json(menu);
+};
 
 exports.adminGetAll = async (req, res) => {};
 
